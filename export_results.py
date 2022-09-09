8a7f8dff632123dbf1cbdccbcd7ed1cbb2ae8736
Wok <woctezuma@users.noreply.github.com>
2022-09-10 00:52:18 +0200

    Add script to export results

---
+++ export_results.py
@@ -0,0 +1,20 @@
+from src.achievements import load_achievements
+from src.rankings import aggregate_ranking, export_ranking_to_csv
+from src.sandboxes import load_sandbox_ids_dict
+from src.trim_utils import trim_achievements
+
+
+def main():
+    sandbox_ids_dict = load_sandbox_ids_dict()
+
+    achievements = load_achievements()
+    achievements = trim_achievements(achievements, export_to_json=False)
+
+    ranking = aggregate_ranking(sandbox_ids_dict, achievements)
+    export_ranking_to_csv(ranking)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
