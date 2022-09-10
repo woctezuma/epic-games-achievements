b6bb90469eb3c2dfc8087659e04deb5baa5bd396
Wok <woctezuma@users.noreply.github.com>
2022-09-10 23:50:26 +0200

    Replace None with N/A for ZeroDivisionError

--- src/player_utils.py
+++ src/player_utils.py
@@ -1,6 +1,6 @@
 def estimate_num_players(num_completionists, platinum_rarity):
     if platinum_rarity == 0:
-        num_players = None
+        num_players = 'N/A'
     else:
         platinum_completion_percentage = platinum_rarity / 100
         num_players = int(num_completionists / platinum_completion_percentage)
