58915c5c0bcdefa49029656f362b019b21146453
Wok <woctezuma@users.noreply.github.com>
2022-09-10 23:27:39 +0200

    Add utils to estimate number of players using platinum stats

---
+++ src/player_utils.py
@@ -0,0 +1,7 @@
+def estimate_num_players(num_completionists, platinum_rarity):
+    if platinum_rarity == 0:
+        num_players = None
+    else:
+        platinum_completion_percentage = platinum_rarity / 100
+        num_players = int(num_completionists / platinum_completion_percentage)
+    return num_players
