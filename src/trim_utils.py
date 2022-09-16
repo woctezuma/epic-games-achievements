e67107cd57ac305f2ceebaa06faa633eda88ccaa
Wok <woctezuma@users.noreply.github.com>
2022-09-16 22:48:31 +0200

    Improve duplicate removal: use text length before lexicographical order

--- src/trim_utils.py
+++ src/trim_utils.py
@@ -23,7 +23,7 @@ def trim_sandbox_ids_dict(sandbox_ids_dict):
     trimmed_dict = dict()
 
     # NB: the sort aims to **try** to make the base edition of the game appear first. This is useful, yet not foolproof!
-    for slug, sandbox_id in sorted(sandbox_ids_dict.items(), key=lambda x: x[0]):
+    for slug, sandbox_id in sorted(sandbox_ids_dict.items(), key=lambda x: (len(x[0]), x[0])):
         # NB: the check for positive length is to tackle the edge case where store data is not on the disk.
         if slug not in slugs_for_base_games and len(slugs_for_base_games) > 0:
             continue
