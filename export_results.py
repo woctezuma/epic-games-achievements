947917978c39716e933fc2df77cd6a94d1b1efcf
Wok <woctezuma@users.noreply.github.com>
2022-09-16 19:53:36 +0200

    Export dict for the tracker, in the script

--- export_results.py
+++ export_results.py
@@ -1,6 +1,7 @@
 from src.achievements import load_achievements
 from src.rankings import aggregate_ranking, export_ranking_to_csv
 from src.sandboxes import load_sandbox_ids_dict
+from src.tracker_utils import export_sandbox_ids_for_tracker
 from src.trim_utils import trim_achievements, trim_sandbox_ids_dict
 
 
@@ -13,6 +14,8 @@ def main():
 
     ranking = aggregate_ranking(sandbox_ids_dict, achievements)
     export_ranking_to_csv(ranking)
+
+    export_sandbox_ids_for_tracker(sandbox_ids_dict, achievements)
 
     return
 
