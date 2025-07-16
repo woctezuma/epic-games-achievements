d8d1a3ab3b5e444eaff67102e591d092f61282ef
Wok <woctezuma@users.noreply.github.com>
2025-07-16 23:48:59 +0200

    Edit GraphQL URL

--- src/api.py
+++ src/api.py
@@ -1,11 +1,11 @@
 import requests
 
 
-def get_graphql_api_url(subdomain="graphql"):
+def get_graphql_api_url(subdomain="store"):
     return f"https://{subdomain}.epicgames.com/graphql"
 
 
-def send_post_request_to_api(json_data, subdomain="graphql", verbose=True):
+def send_post_request_to_api(json_data, subdomain="store", verbose=True):
     r = requests.post(get_graphql_api_url(subdomain=subdomain), json=json_data)
     return to_data(r, verbose=verbose)
 
